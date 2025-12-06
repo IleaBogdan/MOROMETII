e125d973814720c2debb43338c594fc6a78e8cde
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-12-06 03:30:29 +0200

    fuck off luca

--- server/Controllers/UserManager.cs
+++ server/Controllers/UserManager.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using System.Data.SqlClient;
 
 namespace server.Controllers
 {
@@ -7,5 +9,30 @@ namespace server.Controllers
     [ApiController]
     public class UserManager : ControllerBase
     {
+        public static string __connectionString { get; private set; }
+        public static void set_connection(string connectionString)
+        {
+            __connectionString = connectionString;
+        }
+        public class CertificateResponse
+        {
+            public string Error { get; set; }
+            public string Status {  get; set; }
+        }
+
+        [HttpPost]
+        [Route("UploadeCertificate")]
+        [ProducesResponseType(typeof(CertificateResponse), StatusCodes.Status200OK)]
+        public IActionResult UploadeCertificate([FromBody]CertificateData req)
+        {
+            using var connection = new SqlConnection(__connectionString);
+            connection.Open();
+
+            string sql = "";
+
+            using var command = new SqlCommand(sql, connection);
+
+            using var reader = command.ExecuteReader();
+        }
     }
 }
